dbcd5fc1a89d2cdda786c312a7e468da68f15797
MattIRio <matlirioiii@gmail.com>
2024-10-06 12:34:17 +0300

    Initial commit

--- src/main/java/matt/crud_app/controller/StudentController.java
+++ src/main/java/matt/crud_app/controller/StudentController.java
@@ -3,18 +3,20 @@ package matt.crud_app.controller;
 import matt.crud_app.model.Student;
 import matt.crud_app.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin
-@RestController
+@Controller
     @RequestMapping("/student")
     public class StudentController {
         @Autowired
         private StudentService studentService;
 
         @PostMapping("/add")
+        @ResponseBody
         public String add(@RequestBody Student student){
             studentService.saveStudent(student);
             return "student added";
@@ -26,11 +28,13 @@ import java.util.List;
     }
 
         @GetMapping("/getAll")
+        @ResponseBody
     public List<Student> getAllStudents(){
             return studentService.getAllStudents();
         }
 
         @DeleteMapping("/delete/{id}")
+        @ResponseBody
         public String deleteByID(@PathVariable Integer id){
                 studentService.deleteStudent(id);
                 return "student deleted";
